aa6454f375960f7da0532780be9f00f4c8f2f1c6
Runze Li <me@lirunze.xyz>
2018-08-27 17:00:07 +0800

    add local moduls

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk')
+const colors = require('colors')
 
 console.log(chalk.green('NODE_PATH works!'))
 console.log(chalk.yellow('module.paths:'))
 console.log(module.paths)
+console.log(colors.trap('also supports local node_modules as fallback!'))
